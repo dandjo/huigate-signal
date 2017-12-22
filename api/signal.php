1e05f2cc856680cfb16a83b01da5fed46bebc9f3
Daniel Pernold <daniel.pernold@gmail.com>
2017-12-22 18:39:07 +0100

    Parsing xml string on my own (speed)

--- api/signal.php
+++ api/signal.php
@@ -1,5 +1,8 @@
 <?php
-error_reporting(E_ALL);
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+header('Content-Type: application/json');
 
 // login
 $ch = curl_init();
@@ -23,7 +26,7 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'http://homerouter.cpe/api/device/signal');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_COOKIEFILE, "/tmp/cookie.txt");
-$data = curl_exec($ch);
+$xml = str_replace(array("\n", "\r"), '', curl_exec($ch));
 if (curl_error($ch)) {
     header('Content-Type: text/plain');
     echo curl_error($ch);
@@ -32,6 +35,24 @@ if (curl_error($ch)) {
 curl_close($ch);
 
 // output
-header('Content-Type: application/json');
-echo json_encode(simplexml_load_string($data));
+$parse_xml_value = function($xml, $key) {
+    $begin = "<$key>";
+    $end = "</$key>";
+    $begin_pos = mb_strpos($xml, $begin);
+    $end_pos = mb_strpos($xml, $end);
+    $begin_len = mb_strlen($begin);
+    $end_len = mb_strlen($end);
+    return htmlspecialchars_decode(mb_substr(
+        $xml,
+        $begin_pos + mb_strlen($begin),
+        $end_pos - $begin_pos - mb_strlen($end) + 1
+    ), ENT_XML1);
+};
+echo json_encode([
+    'cell_id' => $parse_xml_value($xml, 'cell_id'),
+    'rsrq' => $parse_xml_value($xml, 'rsrq'),
+    'rsrp' => $parse_xml_value($xml, 'rsrp'),
+    'sinr' => $parse_xml_value($xml, 'sinr'),
+    'rssi' => $parse_xml_value($xml, 'rssi'),
+]);
 ?>
